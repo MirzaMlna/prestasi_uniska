6e804a8da03d783851ee570c86bb81f307cb7599
MirzaMlna <mirzamaulana713@gmail.com>
2025-03-03 08:31:51 +0800

    🌐 Bahasa Indonesia

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,14 +11,14 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        Beranda
                     </x-nav-link>
                     {{-- <x-nav-link :href="route('achievements.index')" :active="request()->routeIs('achievements.index')">
-                        {{ __('Data Prestasi') }}
+                        Data Prestasi
                     </x-nav-link> --}}
                     @if (Auth::user()->role === 'admin')
                         <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
-                            {{ __('Data Pengguna') }}
+                            Data Akun
                         </x-nav-link>
                     @endif
                 </div>
