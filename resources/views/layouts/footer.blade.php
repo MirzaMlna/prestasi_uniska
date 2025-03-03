b3b1e059192cb168d85c385cbc05fcd832aa15a3
MirzaMlna <mirzamaulana713@gmail.com>
2025-03-03 09:02:38 +0800

    💄 Add Footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-gray-800 text-white py-4">
+    <div class="container mx-auto text-center">
+        <p class="text-sm">&copy; {{ date('Y') }} Kemahasiswaan UNISKA. Semua Hak Cipta Dilindungi.</p>
+    </div>
+</footer>
